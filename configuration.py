ce874331f7c0c4639a85b9586baad02cabca2b2a
김찬우 <50550972+KimChanw@users.noreply.github.com>
2023-05-05 16:51:28 +0900

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,9 +1,9 @@
 # 데이터베이스 접속 비밀번호
-DB_PASSWORD = 'HTqxlT3g'
-HOST = 'goldsilverprice-db.c4ca5hhwyuky.ap-northeast-2.rds.amazonaws.com'
-USER = 'admin'
+DB_PASSWORD = ''
+HOST = ''
+USER = ''
 
-SECRET_KEY = 'django-insecure--@bw(8tp-_&waq2u(xk_=0g7zl_((e=r*_xb!^n&gpd^-yu!i0'
+SECRET_KEY = ''
 
 DATABASES = {
     'default': {
